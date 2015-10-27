f197c74f804e7d467df792ac9b5abd8c18ecf1d4
Duncan McGregor <duncan@oneeyedmen.com>
2015-10-27 16:05:19 +0000

    Check ordinary items behaviour

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,22 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    public void ordinary_items_decrease_in_quality_one_per_day_up_to_expiry() {
+        checkOneUpdate("foo", 1, 42, 41);
     }
+
+    @Test
+    public void ordinary_items_decrease_in_quality_two_per_day_after_expiry() {
+        checkOneUpdate("foo", 0, 42, 40);
+    }
+
+    private void checkOneUpdate(String itemName, int sellIn, int quality, int expectedQuality) {
+        Item item = new Item(itemName, sellIn, quality);
+        GildedRose app = new GildedRose(new Item[] {item});
+        app.updateQuality();
+        assertEquals(sellIn -1, item.sellIn);
+        assertEquals(expectedQuality, item.quality);
+    }
+
 
 }
