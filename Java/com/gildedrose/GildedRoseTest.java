b7970706253327058dded1d97da9ba3484410db5
Duncan McGregor <duncan@oneeyedmen.com>
2015-10-27 16:17:35 +0000

    Check Backstage Passes

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -28,6 +28,15 @@ public class GildedRoseTest {
         checkOneUpdate("Sulfuras, Hand of Ragnaros", 1, 42, 1, 42);
     }
 
+    @Test
+    public void Backstage_Passes_increase_in_quality_then_decrease() {
+        checkOneUpdate("Backstage passes to a TAFKAL80ETC concert", 11, 15, 10, 16);
+        checkOneUpdate("Backstage passes to a TAFKAL80ETC concert", 10, 15, 9, 17);
+        checkOneUpdate("Backstage passes to a TAFKAL80ETC concert", 5, 15, 4, 18);
+        checkOneUpdate("Backstage passes to a TAFKAL80ETC concert", 1, 15, 0, 18);
+        checkOneUpdate("Backstage passes to a TAFKAL80ETC concert", 0, 15, -1, 0);
+    }
+
     private void checkOneUpdate(String itemName, int sellIn, int quality, int expectedSellIn, int expectedQuality) {
         Item item = new Item(itemName, sellIn, quality);
         GildedRose app = new GildedRose(new Item[] {item});
