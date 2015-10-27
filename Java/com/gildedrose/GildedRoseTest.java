6d19e2b182c8ddfb63cabae890b444cdf0245dda
Duncan McGregor <duncan@oneeyedmen.com>
2015-10-27 16:11:25 +0000

    Check Sulfuras

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -8,26 +8,31 @@ public class GildedRoseTest {
 
     @Test
     public void ordinary_items_decrease_in_quality_one_per_day_up_to_expiry() {
-        checkOneUpdate("foo", 1, 42, 41);
+        checkOneUpdate("foo", 1, 42, 0, 41);
     }
 
     @Test
     public void ordinary_items_decrease_in_quality_two_per_day_after_expiry() {
-        checkOneUpdate("foo", 0, 42, 40);
+        checkOneUpdate("foo", 0, 42, -1, 40);
     }
 
     @Test
     public void Aged_Brie_increases_in_quality_up_to_50() {
-        checkOneUpdate("Aged Brie", 1, 42, 43);
-        checkOneUpdate("Aged Brie", -10, 49, 50);
-        checkOneUpdate("Aged Brie", -11, 50, 50);
+        checkOneUpdate("Aged Brie", 1, 42, 0, 43);
+        checkOneUpdate("Aged Brie", -10, 49, -11, 50);
+        checkOneUpdate("Aged Brie", -11, 50, -12, 50);
     }
 
-    private void checkOneUpdate(String itemName, int sellIn, int quality, int expectedQuality) {
+    @Test
+    public void Sulfuras_doesnt_degrade() {
+        checkOneUpdate("Sulfuras, Hand of Ragnaros", 1, 42, 1, 42);
+    }
+
+    private void checkOneUpdate(String itemName, int sellIn, int quality, int expectedSellIn, int expectedQuality) {
         Item item = new Item(itemName, sellIn, quality);
         GildedRose app = new GildedRose(new Item[] {item});
         app.updateQuality();
-        assertEquals(sellIn -1, item.sellIn);
+        assertEquals(expectedSellIn, item.sellIn);
         assertEquals(expectedQuality, item.quality);
     }
 
