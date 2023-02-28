f58fb577401ccbee8a517c28a3019d81549ada63
Duncan McGregor <duncan@oneeyedmen.com>
2023-02-28 09:27:48 +0000

    bring in methods

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -12,41 +12,38 @@ class GildedRose(
     }
 }
 
-private fun ItemType.update(item: Item) {
-    item.sellIn -= ageingFor()
-    item.degradeBy(degradationFor(item))
-}
-
 enum class ItemType(val description: String) {
     sulfuras("Sulfuras, Hand of Ragnaros"),
     brie("Aged Brie"),
     passes("Backstage passes to a TAFKAL80ETC concert"),
-    other("")
+    other("");
+
+    fun update(item: Item) {
+        item.sellIn -= ageingFor()
+        item.degradeBy(degradationFor(item))
+    }
+
+    private fun ageingFor() =
+        when (this) {
+            sulfuras -> 0
+            else -> 1
+        }
+
+    private fun degradationFor(item: Item) =
+        when (this) {
+            brie -> if (item.sellIn < 0) -2 else -1
+            passes -> when {
+                item.sellIn < 0 -> item.quality
+                item.sellIn < 5 -> -3
+                item.sellIn < 10 -> -2
+                else -> -1
+            }
+            sulfuras -> 0
+            other -> if (item.sellIn < 0) 2 else 1
+        }
 }
 
 fun Item.type() = ItemType.values().find { it.description == this.name } ?: other
-
-
-private fun ItemType.ageingFor() =
-    when (this) {
-        sulfuras -> 0
-        else -> 1
-    }
-
-private fun ItemType.degradationFor(item: Item) =
-    when(this) {
-        brie -> if (item.sellIn < 0) -2 else -1
-        passes -> when {
-            item.sellIn < 0 -> item.quality
-            item.sellIn < 5 -> -3
-            item.sellIn < 10 -> -2
-            else -> -1
-        }
-        sulfuras -> 0
-        other -> if (item.sellIn < 0) 2 else 1
-    }
-
-
 
 private fun Item.degradeBy(change: Int) {
     val newValue = quality - change
