6c7164fa06d6e3f6f8203f91f40b7abf1eed3e89
Duncan McGregor <duncan@oneeyedmen.com>
2023-02-28 08:54:52 +0000

    Tidy

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,7 +3,6 @@ package com.gildedrose
 class GildedRose(
     private val items: List<Item>
 ) {
-
     fun updateQuality() {
         for (item in items) {
             item.update()
@@ -13,9 +12,7 @@ class GildedRose(
 
 private fun Item.update() {
     sellIn -= ageingFor(this)
-    degradeBy(
-        degradationFor(this)
-    )
+    degradeBy(degradationFor(this))
 }
 
 private fun ageingFor(item: Item) =
@@ -26,26 +23,15 @@ private fun ageingFor(item: Item) =
 
 private fun degradationFor(item: Item) =
     when (item.name) {
-        "Aged Brie" -> {
-            if (item.sellIn < 0) -2 else -1
+        "Aged Brie" -> if (item.sellIn < 0) -2 else -1
+        "Backstage passes to a TAFKAL80ETC concert" -> when {
+            item.sellIn < 0 -> item.quality
+            item.sellIn < 5 -> -3
+            item.sellIn < 10 -> -2
+            else -> -1
         }
-
-        "Backstage passes to a TAFKAL80ETC concert" -> {
-            when {
-                item.sellIn < 0 -> item.quality
-                item.sellIn < 5 -> -3
-                item.sellIn < 10 -> -2
-                else -> -1
-            }
-        }
-
-        "Sulfuras, Hand of Ragnaros" -> {
-            0
-        }
-
-        else -> {
-            if (item.sellIn < 0) 2 else 1
-        }
+        "Sulfuras, Hand of Ragnaros" -> 0
+        else -> if (item.sellIn < 0) 2 else 1
     }
 
 
