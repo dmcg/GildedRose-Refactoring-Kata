d55a723343c3058b619daa72a921b9e592f4c8a5
Duncan McGregor <duncan@oneeyedmen.com>
2025-04-14 13:02:40 +0100

    Bring build up to date

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,7 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-	kotlin("jvm") version "1.8.10"
+	kotlin("jvm") version "2.1.20"
+	application
 }
 
 group = "com.gildedrose"
@@ -11,19 +10,31 @@ repositories {
 	mavenCentral()
 }
 
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(8)
+	}
+}
+
 dependencies {
 	implementation(kotlin("stdlib"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+	testImplementation(kotlin("test"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
 }
 
 tasks.test {
 	useJUnitPlatform()
-	testLogging {
-		events("passed", "skipped", "failed")
 	}
+
+tasks.register<JavaExec>("texttest") {
+	description = "Allow you to run text-based approval tests with texttest"
+	group = JavaBasePlugin.BUILD_TASK_NAME
+	mainClass.set("com.gildedrose.TexttestFixtureKt")
+	classpath = sourceSets["test"].runtimeClasspath
+	args("30")
 }
 
-// config JVM target to 1.8 for kotlin compilation tasks
-tasks.withType<KotlinCompile>().configureEach {
-	kotlinOptions.jvmTarget = "1.8"
+
+application {
+	mainClass.set("com.gildedrose.TexttestFixtureKt")
 }
